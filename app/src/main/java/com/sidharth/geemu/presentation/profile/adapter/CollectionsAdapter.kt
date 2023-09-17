22667fd593460167f954810f473ba366d8c62e7d
SidharthMudgil <68889544+SidharthMudgil@users.noreply.github.com>
2023-09-17 15:40:01 +0530

    implemented CollectionsAdapter

---
+++ app/src/main/java/com/sidharth/geemu/presentation/profile/adapter/CollectionsAdapter.kt
@@ -0,0 +1,46 @@
+package com.sidharth.geemu.presentation.profile.adapter
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView.Adapter
+import androidx.recyclerview.widget.RecyclerView.HORIZONTAL
+import androidx.recyclerview.widget.RecyclerView.ViewHolder
+import com.sidharth.geemu.databinding.ItemSectionGamesBinding
+import com.sidharth.geemu.domain.model.Collection
+
+class CollectionsAdapter(
+    private val collections: List<Collection>
+) : Adapter<CollectionsAdapter.CollectionViewHolder>() {
+
+    inner class CollectionViewHolder(
+        private val binding: ItemSectionGamesBinding
+    ) : ViewHolder(binding.root) {
+        fun bind(collection: Collection) {
+            binding.apply {
+                tvLabel.text = collection.name
+                rvGames.layoutManager = LinearLayoutManager(
+                    binding.root.context, HORIZONTAL, false
+                )
+                rvGames.adapter = SavedGamesAdapter(collection.games)
+            }
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CollectionViewHolder {
+        val binding = ItemSectionGamesBinding.inflate(
+            LayoutInflater.from(parent.context), parent, false
+        )
+
+        return CollectionViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: CollectionViewHolder, position: Int) {
+        holder.bind(collections[position])
+    }
+
+    override fun getItemCount(): Int {
+        return collections.size
+    }
+
+}
