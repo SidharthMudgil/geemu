f88911b9466127504eb374368a51a7d625cddea2
SidharthMudgil <68889544+SidharthMudgil@users.noreply.github.com>
2023-09-17 16:48:38 +0530

    implemented ProfileViewModel

--- app/src/main/java/com/sidharth/geemu/presentation/profile/ProfileFragment.kt
+++ app/src/main/java/com/sidharth/geemu/presentation/profile/ProfileFragment.kt
@@ -1,23 +1,45 @@
 package com.sidharth.geemu.presentation.profile
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.sidharth.geemu.R
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView.VERTICAL
+import com.sidharth.geemu.databinding.FragmentProfileBinding
+import com.sidharth.geemu.domain.model.Game
+import com.sidharth.geemu.presentation.profile.adapter.CollectionsAdapter
+import com.sidharth.geemu.presentation.profile.callback.OnGameClickCallback
+import com.sidharth.geemu.presentation.profile.viewmodel.ProfileViewModel
+import dagger.hilt.android.AndroidEntryPoint
 
-class ProfileFragment : Fragment() {
+@AndroidEntryPoint
+class ProfileFragment : Fragment(), OnGameClickCallback {
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-    }
+    private val profileViewModel by viewModels<ProfileViewModel>()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_profile, container, false)
+    ): View {
+        val binding = FragmentProfileBinding.inflate(inflater)
+
+        binding.rvCollections.layoutManager = LinearLayoutManager(
+            requireContext(), VERTICAL, false
+        )
+
+        profileViewModel.collections.observe(viewLifecycleOwner) {
+            binding.rvCollections.adapter = CollectionsAdapter(
+                collections = it,
+                onGameClickCallback = this
+            )
+        }
+
+        return binding.root
     }
 
+    override fun onGameClick(game: Game) {
+    }
 }
---
+++ app/src/main/java/com/sidharth/geemu/presentation/profile/viewmodel/ProfileViewModel.kt
@@ -0,0 +1,63 @@
+package com.sidharth.geemu.presentation.profile.viewmodel
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.sidharth.geemu.domain.model.Collection
+import com.sidharth.geemu.domain.model.Game
+import com.sidharth.geemu.domain.usecase.collection.CollectionUseCase
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class ProfileViewModel @Inject constructor(
+    private val collectionUseCase: CollectionUseCase
+) : ViewModel() {
+
+    private val _collections = MutableLiveData(
+        listOf(
+            Collection(0, "Uncategorized", emptyList()),
+            Collection(1, "Playing", emptyList()),
+            Collection(2, "Not Played", emptyList()),
+            Collection(3, "Completed", emptyList()),
+            Collection(4, "Played", emptyList()),
+        )
+    )
+
+    val collections: LiveData<List<Collection>> get() = _collections
+
+    init {
+        updateCollectionsData()
+    }
+
+    private fun updateCollectionsData() {
+        viewModelScope.launch {
+            _collections.value = collectionUseCase.getGameCollections()
+        }
+    }
+
+    fun addGameToCollection(game: Game, collection: Int) {
+        viewModelScope.launch {
+            collectionUseCase.addGameToCollection(game, collection)
+            updateCollectionsData()
+        }
+    }
+
+    fun moveGameToCollection(game: Game, collection: Int) {
+        viewModelScope.launch {
+            collectionUseCase.moveGameToCollection(game, collection)
+            updateCollectionsData()
+        }
+    }
+
+    fun removeGameFromCollection(game: Game) {
+        viewModelScope.launch {
+            collectionUseCase.removeGameFromCollections(game)
+            updateCollectionsData()
+        }
+    }
+}
