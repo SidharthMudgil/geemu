e0af7b814443c826ff3bd01ea81517c56c1784fa
SidharthMudgil <68889544+SidharthMudgil@users.noreply.github.com>
2023-09-17 15:13:46 +0530

    implemented adapters for HomeFragment

--- app/src/main/java/com/sidharth/geemu/presentation/home/HomeFragment.kt
+++ app/src/main/java/com/sidharth/geemu/presentation/home/HomeFragment.kt
@@ -1,23 +1,58 @@
 package com.sidharth.geemu.presentation.home
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.sidharth.geemu.R
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView.VERTICAL
+import com.sidharth.geemu.databinding.FragmentHomeBinding
+import com.sidharth.geemu.domain.model.Game
+import com.sidharth.geemu.domain.model.Genre
+import com.sidharth.geemu.presentation.home.adapter.HomeAdapter
+import com.sidharth.geemu.presentation.home.callback.OnGameClickCallback
+import com.sidharth.geemu.presentation.home.callback.OnGenreClickCallback
+import com.sidharth.geemu.presentation.home.callback.OnSearchBarClickCallback
+import com.sidharth.geemu.presentation.home.viewmodel.HomeViewModel
+import dagger.hilt.android.AndroidEntryPoint
 
-class HomeFragment : Fragment() {
+@AndroidEntryPoint
+class HomeFragment : Fragment(),
+    OnSearchBarClickCallback, OnGenreClickCallback, OnGameClickCallback {
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-    }
+    private val homeViewModel by viewModels<HomeViewModel>()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_home, container, false)
+    ): View {
+        val binding = FragmentHomeBinding.inflate(inflater)
+
+        binding.rvHome.layoutManager = LinearLayoutManager(requireContext(), VERTICAL, false)
+        homeViewModel.homeData.observe(viewLifecycleOwner) {
+            binding.rvHome.adapter = HomeAdapter(
+                onSearchBarClickCallback = this,
+                onGenreClickCallback = this,
+                onGameClickCallback = this,
+                genres = it.genres,
+                upcoming = it.upcoming,
+                bestOfYear = it.bestOfYear,
+                bestOfAllTime = it.bestOfAllTime,
+            )
+        }
+
+        return binding.root
+    }
+
+    override fun onGameClick(game: Game) {
+    }
+
+    override fun onGenreClick(genre: Genre) {
+    }
+
+    override fun onClick() {
     }
 
 }
