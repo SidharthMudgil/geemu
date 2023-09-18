a6b8b1e1e403a3677ff7cf8af8f16516313b607f
SidharthMudgil <68889544+SidharthMudgil@users.noreply.github.com>
2023-09-18 12:49:17 +0530

    implemented logic for populating ui with data for FollowingFragment

--- app/src/main/java/com/sidharth/geemu/presentation/following/FollowingFragment.kt
+++ app/src/main/java/com/sidharth/geemu/presentation/following/FollowingFragment.kt
@@ -5,13 +5,22 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView.VERTICAL
 import com.sidharth.geemu.databinding.FragmentFollowingBinding
+import com.sidharth.geemu.domain.model.Game
+import com.sidharth.geemu.domain.model.Tag
+import com.sidharth.geemu.presentation.following.adapter.FollowingAdapter
+import com.sidharth.geemu.presentation.following.callback.OnGameClickCallback
+import com.sidharth.geemu.presentation.following.callback.OnUnfollowButtonClickCallback
+import com.sidharth.geemu.presentation.following.viewmodel.FollowingViewModel
+import dagger.hilt.android.AndroidEntryPoint
 
-class FollowingFragment: Fragment() {
+@AndroidEntryPoint
+class FollowingFragment: Fragment(), OnGameClickCallback, OnUnfollowButtonClickCallback {
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-    }
+    private val followingViewModel by viewModels<FollowingViewModel>()
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -20,6 +29,19 @@ class FollowingFragment: Fragment() {
     ): View {
         val binding = FragmentFollowingBinding.inflate(inflater)
 
+        binding.rvFollowing.layoutManager = LinearLayoutManager(requireContext(), VERTICAL, false)
+        followingViewModel.following.observe(viewLifecycleOwner) {
+            binding.rvFollowing.adapter = FollowingAdapter(it, this, this)
+        }
+
         return binding.root
+    }
+
+    override fun onGameClick(game: Game) {
+
+    }
+
+    override fun onUnfollowButtonClick(tag: Tag) {
+        followingViewModel.unfollowTag(tag)
     }
 }
--- app/src/main/java/com/sidharth/geemu/presentation/following/viewmodel/FollowingViewModel.kt
+++ app/src/main/java/com/sidharth/geemu/presentation/following/viewmodel/FollowingViewModel.kt
@@ -1,22 +1,26 @@
 package com.sidharth.geemu.presentation.following.viewmodel
 
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.sidharth.geemu.domain.model.Tag
+import com.sidharth.geemu.domain.usecase.game.GameUseCase
 import com.sidharth.geemu.domain.usecase.tag.TagUseCase
+import com.sidharth.geemu.presentation.following.Following
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
 class FollowingViewModel @Inject constructor(
-    private val tagUseCase: TagUseCase
-): ViewModel() {
+    private val tagUseCase: TagUseCase,
+    private val gameUseCase: GameUseCase,
+) : ViewModel() {
 
-    private val _following = MutableLiveData<List<Tag>>()
+    private val _following = MutableLiveData<List<Following>>()
 
-    val following get() = _following
+    val following: LiveData<List<Following>> get() = _following
 
     init {
         fetchFollowing()
@@ -24,21 +28,43 @@ class FollowingViewModel @Inject constructor(
 
     private fun fetchFollowing() {
         viewModelScope.launch {
-            _following.postValue(tagUseCase.getTags())
+            val followings = mutableListOf<Following>()
+
+            tagUseCase.getTags().forEach {
+                followings.add(
+                    Following(
+                        tag = it,
+                        games = gameUseCase.getGamesByTags(it.id.toString())
+                    )
+                )
+            }
+
+            _following.postValue(followings)
         }
     }
 
     fun followTag(tag: Tag) {
         viewModelScope.launch {
             tagUseCase.followTag(tag)
-            fetchFollowing()
+
+            val newFollowing = following.value?.toMutableList() ?: mutableListOf()
+            newFollowing.add(
+                Following(
+                    tag = tag,
+                    games = gameUseCase.getGamesByTags(tag.id.toString()),
+                )
+            )
+            _following.postValue(newFollowing)
         }
     }
 
     fun unfollowTag(tag: Tag) {
         viewModelScope.launch {
             tagUseCase.unfollowTag(tag)
-            fetchFollowing()
+
+            val newFollowing = following.value?.toMutableList() ?: mutableListOf()
+            newFollowing.removeAll { it.tag == tag }
+            _following.postValue(newFollowing)
         }
     }
 }
