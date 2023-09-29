a88fb89f2c560504fbab4380578c0b9105f34d3d
SidharthMudgil <68889544+SidharthMudgil@users.noreply.github.com>
2023-09-29 16:15:11 +0530

    removed SearchFragment

--- app/src/main/java/com/sidharth/geemu/presentation/following/callback/OnUnfollowButtonClickCallback.kt
+++
@@ -1,7 +0,0 @@
-package com.sidharth.geemu.presentation.following.callback
-
-import com.sidharth.geemu.domain.model.Tag
-
-interface OnUnfollowButtonClickCallback {
-    fun onUnfollowButtonClick(tag: Tag)
-}
