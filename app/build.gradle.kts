7137a9ad3826795a6d415222074347724c76cf2a
SidharthMudgil <68889544+SidharthMudgil@users.noreply.github.com>
2023-10-07 14:53:40 +0530

    implemented class GamesPagingSource

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -53,7 +53,7 @@ android {
 dependencies {
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation("com.google.android.material:material:1.9.0")
+    implementation("com.google.android.material:material:1.10.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
@@ -63,10 +63,10 @@ dependencies {
     val lifecycleVersion = "2.6.2"
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:$lifecycleVersion")
     implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:$lifecycleVersion")
-    implementation("androidx.activity:activity-ktx:1.7.2")
+    implementation("androidx.activity:activity-ktx:1.8.0")
 
     // navigation
-    val navigationVersion = "2.7.3"
+    val navigationVersion = "2.7.4"
     implementation("androidx.navigation:navigation-fragment-ktx:$navigationVersion")
     implementation("androidx.navigation:navigation-ui-ktx:$navigationVersion")
 
@@ -87,6 +87,9 @@ dependencies {
 
     // image loading - Coil
     implementation("io.coil-kt:coil:2.4.0")
+
+    // Paging
+    implementation("androidx.paging:paging-common-ktx:3.2.1")
 
     // video - Media3
     val media3Version = "1.1.1"
--- app/src/main/java/com/sidharth/geemu/data/remote/source/GamesPagingSource.kt
+++ app/src/main/java/com/sidharth/geemu/data/remote/source/GamesPagingSource.kt
@@ -1,3 +1,35 @@
 package com.sidharth.geemu.data.remote.source
 
-class GamesPagingSource
+import androidx.paging.PagingSource
+import androidx.paging.PagingState
+import com.sidharth.geemu.data.mapper.ResponseMapper.toGames
+import com.sidharth.geemu.domain.model.Game
+import javax.inject.Inject
+
+class GamesPagingSource @Inject constructor(
+    private val remoteDataSource: RemoteDataSource
+) : PagingSource<Int, Game>() {
+
+    override suspend fun load(params: LoadParams<Int>): LoadResult<Int, Game> {
+        return try {
+            val nextPageNumber = params.key ?: 1
+            val response = remoteDataSource.getGames(
+                page = nextPageNumber,
+                pageSize = 20
+            )
+            val data = response?.toGames() ?: emptyList()
+
+            LoadResult.Page(
+                data = data,
+                prevKey = if (nextPageNumber == 1) null else nextPageNumber - 1,
+                nextKey = if (response?.next == null) null else nextPageNumber + 1
+            )
+        } catch (exception: Exception) {
+            LoadResult.Error(exception)
+        }
+    }
+
+    override fun getRefreshKey(state: PagingState<Int, Game>): Int {
+        return 1
+    }
+}
