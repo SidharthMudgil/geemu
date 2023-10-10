fa86648c9b31060a1795943575316cfe1bb756ab
SidharthMudgil <68889544+SidharthMudgil@users.noreply.github.com>
2023-10-10 16:01:53 +0530

    scrollView replaced with recyclerView for ProfileFragment

--- app/src/main/java/com/sidharth/geemu/presentation/profile/ProfileFragment.kt
+++ app/src/main/java/com/sidharth/geemu/presentation/profile/ProfileFragment.kt
@@ -14,15 +14,9 @@ import androidx.lifecycle.repeatOnLifecycle
 import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView.VERTICAL
-import com.github.aachartmodel.aainfographics.aachartcreator.AAChartModel
-import com.github.aachartmodel.aainfographics.aachartcreator.AAChartType
-import com.github.aachartmodel.aainfographics.aachartcreator.AASeriesElement
-import com.sidharth.geemu.R
 import com.sidharth.geemu.databinding.FragmentProfileBinding
-import com.sidharth.geemu.domain.model.Collection
 import com.sidharth.geemu.domain.model.Game
-import com.sidharth.geemu.domain.model.Tag
-import com.sidharth.geemu.presentation.profile.adapter.CollectionsAdapter
+import com.sidharth.geemu.presentation.profile.adapter.ProfileScreenAdapter
 import com.sidharth.geemu.presentation.profile.callback.OnGameClickCallback
 import com.sidharth.geemu.presentation.viewmodel.UserDataViewModel
 import dagger.hilt.android.AndroidEntryPoint
@@ -39,7 +33,7 @@ class ProfileFragment : Fragment(), OnGameClickCallback {
     ): View {
         val binding = FragmentProfileBinding.inflate(inflater)
 
-        binding.rvCollections.layoutManager = LinearLayoutManager(
+        binding.rvProfile.layoutManager = LinearLayoutManager(
             requireContext(), VERTICAL, false
         )
         viewLifecycleOwner.lifecycleScope.launch {
@@ -51,18 +45,11 @@ class ProfileFragment : Fragment(), OnGameClickCallback {
                         onData(binding)
                     }
 
-                    binding.rvCollections.adapter = CollectionsAdapter(
+                    binding.rvProfile.adapter = ProfileScreenAdapter(
                         collections = collections,
-                        onGameClickCallback = this@ProfileFragment
+                        tags = userDataViewModel.following.value,
+                        onGameClickCallback = this@ProfileFragment,
                     )
-
-                    generateGenresDistributionChart(collections).apply {
-                        binding.chartGenresDistribution.aa_drawChartWithChartModel(this)
-                    }
-
-                    generateCollectionOverviewChart(collections).apply {
-                        binding.chartCollectionOverview.aa_drawChartWithChartModel(this)
-                    }
                 }
             }
         }
@@ -70,9 +57,11 @@ class ProfileFragment : Fragment(), OnGameClickCallback {
         viewLifecycleOwner.lifecycleScope.launch {
             repeatOnLifecycle(Lifecycle.State.STARTED) {
                 userDataViewModel.following.collect { tags ->
-                    generatePopularTagsChart(tags).apply {
-                        binding.chartPopularTags.aa_drawChartWithChartModel(this)
-                    }
+                    binding.rvProfile.adapter = ProfileScreenAdapter(
+                        collections = userDataViewModel.collections.value,
+                        tags = tags,
+                        onGameClickCallback = this@ProfileFragment,
+                    )
                 }
             }
         }
@@ -80,92 +69,17 @@ class ProfileFragment : Fragment(), OnGameClickCallback {
         return binding.root
     }
 
-    private fun getGenresDistributionMap(collections: List<Collection>): Map<String, Int> {
-        val genreCountMap = collections
-            .flatMap { it.games }
-            .flatMap { it.genres.split(", ") }
-            .groupingBy { it }
-            .eachCount()
-            .toList().sortedByDescending { it.second }
-
-        val topGenres = genreCountMap.take(4)
-        val other = genreCountMap.drop(4).sumOf { it.second }
-        return if (other > 0) {
-            (topGenres + Pair("Others", other)).toMap()
-        } else topGenres.toMap()
-    }
-
-    private fun generateGenresDistributionChart(collections: List<Collection>): AAChartModel {
-        val data = getGenresDistributionMap(collections)
-
-        return AAChartModel()
-            .chartType(AAChartType.Pie)
-            .backgroundColor(R.color.grey900)
-            .series(
-                arrayOf(
-                    AASeriesElement().name("Game Count").data(
-                        data.map { arrayOf(it.key, it.value) }.toTypedArray()
-                    )
-                )
-            )
-    }
-
-    private fun generateCollectionOverviewChart(collections: List<Collection>): AAChartModel {
-        return AAChartModel()
-            .chartType(AAChartType.Bar)
-            .backgroundColor(R.color.grey900)
-            .categories(collections.map { it.name }.toTypedArray())
-            .series(
-                arrayOf(
-                    AASeriesElement().name("Game Count").data(
-                        collections.map { arrayOf(it.name, it.games.size) }
-                            .toTypedArray()
-                    )
-                )
-            )
-    }
-
-    private fun generatePopularTagsChart(tags: List<Tag>): AAChartModel {
-        val popular = tags.sortedByDescending { it.count }.take(10).sortedBy { it.name }
-
-        return AAChartModel()
-            .chartType(AAChartType.Column)
-            .backgroundColor(R.color.grey900)
-            .categories(popular.map { it.name }.toTypedArray())
-            .series(
-                arrayOf(
-                    AASeriesElement().name("Game Count").data(
-                        popular.map {
-                            arrayOf(it.name, it.count)
-                        }.toTypedArray()
-                    )
-                )
-            )
-    }
-
     private fun onNoData(binding: FragmentProfileBinding) {
         binding.apply {
-            loading.visibility = VISIBLE
-            rvCollections.visibility = GONE
-            tvPopularTags.visibility = GONE
-            chartPopularTags.visibility = GONE
-            tvCollectionOverview.visibility = GONE
-            chartCollectionOverview.visibility = GONE
-            tvGenresDistribution.visibility = GONE
-            chartGenresDistribution.visibility = GONE
+            flowNoData.visibility = VISIBLE
+            rvProfile.visibility = GONE
         }
     }
 
     private fun onData(binding: FragmentProfileBinding) {
         binding.apply {
-            loading.visibility = GONE
-            rvCollections.visibility = VISIBLE
-            tvPopularTags.visibility = VISIBLE
-            chartPopularTags.visibility = VISIBLE
-            tvCollectionOverview.visibility = VISIBLE
-            chartCollectionOverview.visibility = VISIBLE
-            tvGenresDistribution.visibility = VISIBLE
-            chartGenresDistribution.visibility = VISIBLE
+            flowNoData.visibility = GONE
+            rvProfile.visibility = VISIBLE
         }
     }
 
---
+++ app/src/main/java/com/sidharth/geemu/presentation/profile/adapter/ProfileScreenAdapter.kt
@@ -0,0 +1,182 @@
+package com.sidharth.geemu.presentation.profile.adapter
+
+import android.view.LayoutInflater
+import android.view.View.GONE
+import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import androidx.recyclerview.widget.RecyclerView.Adapter
+import androidx.recyclerview.widget.RecyclerView.ViewHolder
+import com.github.aachartmodel.aainfographics.aachartcreator.AAChartModel
+import com.github.aachartmodel.aainfographics.aachartcreator.AAChartType
+import com.github.aachartmodel.aainfographics.aachartcreator.AASeriesElement
+import com.sidharth.geemu.R
+import com.sidharth.geemu.databinding.ItemSectionChartBinding
+import com.sidharth.geemu.databinding.ItemSectionCollectionBinding
+import com.sidharth.geemu.domain.model.Collection
+import com.sidharth.geemu.domain.model.Tag
+import com.sidharth.geemu.presentation.profile.callback.OnGameClickCallback
+
+class ProfileScreenAdapter(
+    private val collections: List<Collection>,
+    private val tags: List<Tag>,
+    private val onGameClickCallback: OnGameClickCallback,
+) : Adapter<ViewHolder>() {
+    enum class ProfileSection {
+        COLLECTION, CHART
+    }
+
+    enum class ChartType {
+        GENRES, COLLECTION, TAGS
+    }
+
+    inner class CollectionsViewHolder(
+        private val binding: ItemSectionCollectionBinding
+    ) : ViewHolder(binding.root) {
+        fun bind() {
+            binding.rvCollection.layoutManager = LinearLayoutManager(
+                binding.root.context, RecyclerView.VERTICAL, false
+            )
+            binding.rvCollection.adapter = CollectionsAdapter(
+                collections = collections,
+                onGameClickCallback = onGameClickCallback
+            )
+        }
+    }
+
+    inner class ChartViewHolder(
+        private val binding: ItemSectionChartBinding
+    ) : ViewHolder(binding.root) {
+        fun bind(type: ChartType) {
+            when (type) {
+                ChartType.GENRES -> {
+                    generateGenresDistributionChart().apply {
+                        binding.tvLabel.text =
+                            binding.root.context.getString(R.string.genres_distribution)
+                        binding.chart.aa_drawChartWithChartModel(this)
+                    }
+                }
+
+                ChartType.COLLECTION -> {
+                    binding.tvLabel.text =
+                        binding.root.context.getString(R.string.collection_overview)
+                    generateCollectionOverviewChart().apply {
+                        binding.chart.aa_drawChartWithChartModel(this)
+                    }
+                }
+
+                ChartType.TAGS -> {
+                    if (tags.isNotEmpty()) {
+                        binding.tvLabel.text = binding.root.context.getString(R.string.popular_tags)
+                        generatePopularTagsChart().apply {
+                            binding.chart.aa_drawChartWithChartModel(this)
+                        }
+                    } else {
+                        binding.tvLabel.visibility = GONE
+                        binding.chart.visibility = GONE
+                    }
+                }
+            }
+        }
+
+        private fun generateGenresDistributionChart(): AAChartModel {
+            val genreCountMap = collections
+                .flatMap { it.games }
+                .flatMap { it.genres.split(", ") }
+                .groupingBy { it }
+                .eachCount()
+                .toList().sortedByDescending { it.second }
+
+            val topGenres = genreCountMap.take(4)
+            val other = genreCountMap.drop(4).sumOf { it.second }
+            val data = if (other > 0) {
+                (topGenres + Pair("Others", other)).toMap()
+            } else topGenres.toMap()
+
+            return AAChartModel()
+                .chartType(AAChartType.Pie)
+                .backgroundColor(R.color.grey900)
+                .series(
+                    arrayOf(
+                        AASeriesElement().name("Game Count").data(
+                            data.map { arrayOf(it.key, it.value) }.toTypedArray()
+                        )
+                    )
+                )
+        }
+
+        private fun generateCollectionOverviewChart(): AAChartModel {
+            return AAChartModel()
+                .chartType(AAChartType.Bar)
+                .backgroundColor(R.color.grey900)
+                .categories(collections.map { it.name }.toTypedArray())
+                .series(
+                    arrayOf(
+                        AASeriesElement().name("Game Count").data(
+                            collections.map { arrayOf(it.name, it.games.size) }
+                                .toTypedArray()
+                        )
+                    )
+                )
+        }
+
+        private fun generatePopularTagsChart(): AAChartModel {
+            val popular = tags.sortedByDescending { it.count }.take(10).sortedBy { it.name }
+
+            return AAChartModel()
+                .chartType(AAChartType.Column)
+                .backgroundColor(R.color.grey900)
+                .categories(popular.map { it.name }.toTypedArray())
+                .series(
+                    arrayOf(
+                        AASeriesElement().name("Game Count").data(
+                            popular.map {
+                                arrayOf(it.name, it.count)
+                            }.toTypedArray()
+                        )
+                    )
+                )
+        }
+    }
+
+    override fun getItemViewType(position: Int): Int {
+        return when (position) {
+            0 -> ProfileSection.COLLECTION.ordinal
+            1, 2, 3 -> ProfileSection.CHART.ordinal
+            else -> throw IllegalStateException("Invalid Type")
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val inflater = LayoutInflater.from(parent.context)
+
+        return when (viewType) {
+            ProfileSection.COLLECTION.ordinal -> CollectionsViewHolder(
+                ItemSectionCollectionBinding.inflate(
+                    inflater, parent, false
+                )
+            )
+
+            ProfileSection.CHART.ordinal -> ChartViewHolder(
+                ItemSectionChartBinding.inflate(
+                    inflater, parent, false
+                )
+            )
+
+            else -> throw IllegalStateException("Invalid Type")
+        }
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        when (position) {
+            0 -> (holder as CollectionsViewHolder).bind()
+            1 -> (holder as ChartViewHolder).bind(ChartType.GENRES)
+            2 -> (holder as ChartViewHolder).bind(ChartType.COLLECTION)
+            3 -> (holder as ChartViewHolder).bind(ChartType.TAGS)
+        }
+    }
+
+    override fun getItemCount(): Int {
+        return 4
+    }
+}
