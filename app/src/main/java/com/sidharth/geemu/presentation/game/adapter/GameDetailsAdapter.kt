95ad898892563ee00d477b43253dd9765c7b1bc5
SidharthMudgil <68889544+SidharthMudgil@users.noreply.github.com>
2023-10-13 19:46:15 +0530

    update ui of item_card_game4

--- app/src/main/java/com/sidharth/geemu/presentation/game/adapter/GameDetailsAdapter.kt
+++ app/src/main/java/com/sidharth/geemu/presentation/game/adapter/GameDetailsAdapter.kt
@@ -2,6 +2,7 @@ package com.sidharth.geemu.presentation.game.adapter
 
 import android.annotation.SuppressLint
 import android.text.Html
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View.GONE
 import android.view.ViewGroup
@@ -66,6 +67,7 @@ class GameDetailsAdapter(
                 }
                 tvDescription.text =
                     Html.fromHtml(gameDetails.description, Html.FROM_HTML_MODE_LEGACY)
+                Log.d("description", gameDetails.description)
                 tvRatings.text = "(${gameDetails.rating})"
                 gameDetails.platforms.forEach {
                     val chip = Chip(root.context)
