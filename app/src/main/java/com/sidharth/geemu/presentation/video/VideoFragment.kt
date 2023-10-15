ee63a9bd611ef1179108e7471c4e89a5f8fe5133
SidharthMudgil <68889544+SidharthMudgil@users.noreply.github.com>
2023-10-15 16:29:20 +0530

    add media3 api

--- app/src/main/java/com/sidharth/geemu/presentation/video/VideoFragment.kt
+++ app/src/main/java/com/sidharth/geemu/presentation/video/VideoFragment.kt
@@ -1,16 +1,14 @@
 package com.sidharth.geemu.presentation.video
 
-import android.net.Uri
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.navArgs
-import com.devbrackets.android.exomedia.listener.OnPreparedListener
 import com.sidharth.geemu.databinding.FragmentVideoBinding
 
-class VideoFragment : Fragment(), OnPreparedListener {
+class VideoFragment : Fragment() {
     private val args: VideoFragmentArgs by navArgs()
     private lateinit var binding: FragmentVideoBinding
 
@@ -20,17 +18,7 @@ class VideoFragment : Fragment(), OnPreparedListener {
     ): View {
         binding = FragmentVideoBinding.inflate(inflater)
 
-        setupVideoView()
 
         return binding.root
-    }
-
-    private fun setupVideoView() {
-        binding.videoView.setOnPreparedListener(this)
-        binding.videoView.setMedia(Uri.parse(args.high))
-    }
-
-    override fun onPrepared() {
-        binding.videoView.start()
     }
 }
