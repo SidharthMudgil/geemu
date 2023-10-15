7ba8a1d10b656ba461be7f3af688146f5d6f9b22
SidharthMudgil <68889544+SidharthMudgil@users.noreply.github.com>
2023-10-15 20:19:54 +0530

    implement player & playerView

--- app/src/main/java/com/sidharth/geemu/presentation/video/VideoFragment.kt
+++ app/src/main/java/com/sidharth/geemu/presentation/video/VideoFragment.kt
@@ -5,6 +5,8 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.media3.common.MediaItem
+import androidx.media3.exoplayer.ExoPlayer
 import androidx.navigation.fragment.navArgs
 import com.sidharth.geemu.databinding.FragmentVideoBinding
 
@@ -12,13 +14,34 @@ class VideoFragment : Fragment() {
     private val args: VideoFragmentArgs by navArgs()
     private lateinit var binding: FragmentVideoBinding
 
+    private var player: ExoPlayer? = null
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
         binding = FragmentVideoBinding.inflate(inflater)
 
+        initPlayer()
 
         return binding.root
+    }
+
+    override fun onStop() {
+        super.onStop()
+        releasePlayer()
+    }
+
+    private fun initPlayer() {
+        player = ExoPlayer.Builder(requireContext()).build().apply {
+            binding.playerView.player = this
+            setMediaItem(MediaItem.fromUri(args.high))
+            prepare()
+            play()
+        }
+    }
+
+    private fun releasePlayer() {
+        player?.release()
     }
 }
