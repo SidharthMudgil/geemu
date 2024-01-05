26bfdff443a90b0d695a8e0410596248243654dc
Sidharth Mudgil <68889544+SidharthMudgil@users.noreply.github.com>
2024-01-05 19:21:35 +0530

    change min sdk to 27

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,7 +15,7 @@ android {
 
     defaultConfig {
         applicationId = "com.sidharth.geemu"
-        minSdk = 30
+        minSdk = 27
         targetSdk = 34
         versionCode = 1
         versionName = "1.0.0"
