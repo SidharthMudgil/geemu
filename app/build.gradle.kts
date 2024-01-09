b9348438ab9015efa3b3f8bf89d986b5fc4eef58
Sidharth Mudgil <68889544+SidharthMudgil@users.noreply.github.com>
2024-01-09 12:24:50 +0530

    fix issue: action_exploreFragment_to_noNetworkFragment cannot be found

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,8 +17,8 @@ android {
         applicationId = "com.sidharth.geemu"
         minSdk = 27
         targetSdk = 34
-        versionCode = 1
-        versionName = "1.0.0"
+        versionCode = 2
+        versionName = "1.0.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         buildConfigField(
